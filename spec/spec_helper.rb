0a5e529f7d6b7fc840cea4282717852808a5d13c
Londerson <londerson@gmail.com>
2016-09-16 00:40:56 -0300

    create spec helper

---
+++ spec/spec_helper.rb
@@ -0,0 +1,14 @@
+require './lib/routes'
+
+RSpec.configure do |config|
+  $LOAD_PATH << "../lib"
+
+  # Use color in STDOUT
+  config.color = true
+
+  # Use color not only in STDOUT but also in pagers and files
+  config.tty = true
+
+  # Use the specified formatter
+  config.formatter = :documentation # :progress, :html, :textmate
+end
