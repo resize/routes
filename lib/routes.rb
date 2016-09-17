8cac2821e97d77bea7d8fece27ce693d709bb270
Londerson <londerson@gmail.com>
2016-09-17 18:50:46 -0300

    require files

--- lib/routes.rb
+++ lib/routes.rb
@@ -1,6 +1,8 @@
 require_relative 'routes/route'
 require_relative 'routes/format'
 require_relative 'routes/attr'
+require_relative 'routes/collection'
+require_relative 'routes/recognize'
 
 module Routes
 end
