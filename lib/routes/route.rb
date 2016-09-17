2a39ed0622826ed57d732772c1794fb37d72bbb2
Londerson <londerson@gmail.com>
2016-09-17 18:51:04 -0300

    ident

--- lib/routes/route.rb
+++ lib/routes/route.rb
@@ -1,12 +1,13 @@
 module Routes
   
-  ##
   # This file is part of the Resize module
   # (c) Londerson Araújo <londerson@gmail.com>
   # For the full copyright and license information, please view the LICENSE
   # file that was distributed with this source code.
+  #
 
   class Route
+
     def get(*args)
       attrs("GET", *args)
     end
